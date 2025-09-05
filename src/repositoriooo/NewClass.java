3cf7e43afd0af6ce7a16320ffb9b9e4cbba1a717
Samantha <samseb1724@gmail.com>
2025-09-05 09:10:02 -0500

    Segundo commit

---
+++ src/repositoriooo/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositoriooo;
+
+/**
+ *
+ * @author MI PC
+ */
+public class NewClass {
+    
+    public static String hola(String frase){
+        return frase + "hola";
+    }
+}
